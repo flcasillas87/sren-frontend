e23351110e636e84e7958dc0baad3a503c08ca8b
Fernando Lopez Casillas <39224573+flcasillas87@users.noreply.github.com>
2025-03-26 23:08:15 -0600

    Create consumption.model.ts

---
+++ src/app/core/models/consumption.model.ts
@@ -0,0 +1,9 @@
+
+// models/consumption.model.ts
+export interface Consumption {
+  id: string;
+  date: Date;
+  central: string; // ID de la central generadora
+  volume: number; // m³
+  unit: 'BTU' | 'MWh';
+}

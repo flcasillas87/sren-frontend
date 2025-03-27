568e134e48db51f0e7f2a5385b779a42104d0fa8
Fernando Lopez Casillas <39224573+flcasillas87@users.noreply.github.com>
2025-03-26 23:01:52 -0600

    Update precios.service.ts

--- src/app/core/services/precios.service.ts
+++ src/app/core/services/precios.service.ts
@@ -1,26 +1,13 @@
-import { Injectable, inject } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { Precio } from '../models/precios.model';
-@Injectable({
-  providedIn: 'root',
-})
-export class PreciosService {
-  constructor() {}
+// features/prices/services/price.service.ts
+import { inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-  //Metodos para obtener, crear, actualizar y eliminar
-  //Considera el uso de RxJS Observables para manejar la asincronía en tus modelos de datos.
-  //validar datos en los modelos o en los servicios antes de enviarlos al servidor o de utilizarlos en tu aplicación.
-  getPrecio(): Observable<Precio[]> {
-    // Aquí puedes hacer una llamada HTTP a una API para obtener los datos de los empleados.
-    // Por simplicidad, retornamos datos estáticos en este ejemplo.
-    //Dummy Data
-    const precios: Precio[] = [
-      new Precio(1, 'NRS', 'CC Noreste', '15/05/2023', 150),
-      new Precio(2, 'NRS', 'CC Noreste', '16/05/2023', 200),
-      new Precio(3, 'NRS', 'CC Noreste', '17/05/2023', 250),
-    ];
-    //
+export const priceService = () => {
+  const http = inject(HttpClient);
+  const apiUrl = '/api/prices';
 
-    return of(precios);
-  }
-}
+  return {
+    getPrices: () => http.get<Price[]>(apiUrl),
+    addPrice: (price: Price) => http.post<Price>(apiUrl, price)
+  };
+};

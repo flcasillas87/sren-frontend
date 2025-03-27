6fec119cbe63a880fef58e35ba59cc106fcacf7f
flcasillas87 <flcasillas@gmail.com>
2025-03-27 09:30:45 -0600

    Add PriceForm component with template, styles, and tests; remove obsolete Read.me file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router'; // Importa Routes para definir las rutas de la aplicación.
 import { LayoutComponent } from './shared/components/layout/layout.component'; // Importa el componente Layout
-import { FooterComponent } from './shared/components/footer/footer.component';
+import { PriceFormComponent } from './features/precios/componets/price-form/price-form.component'; // Importa el componente PriceFormComponent
+
 
 // Define las rutas de la aplicación.
 export const routes: Routes = [
   {
     path: '',
-    component: FooterComponent, 
+    component: PriceFormComponent, 
   },
   { 
     path: '**', 
---
+++ src/app/features/precios/componets/price-form/price-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-price-form',
+  imports: [],
+  templateUrl: './price-form.component.html',
+  styleUrl: './price-form.component.css'
+})
+export class PriceFormComponent {
+
+}

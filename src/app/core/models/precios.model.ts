d0aa308aa396ff1df7c969069c8069f05097355b
Fernando Lopez Casillas <39224573+flcasillas87@users.noreply.github.com>
2025-03-26 22:39:28 -0600

    Update precios.model.ts

--- src/app/core/models/precios.model.ts
+++ src/app/core/models/precios.model.ts
@@ -1,9 +1,7 @@
-export class Precio {
-  constructor(
-    public id?: number,
-    public name?: string,
-    public central?: string,
-    public fecha?: string,
-    public valor?: number
-  ) {}
-}
+
+   export interface Price {
+     date: Date;
+     value: number;
+     currency: string;
+     provider: string;
+   }

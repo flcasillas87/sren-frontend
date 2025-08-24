41e92afa24be14f42169fc0f87b825fe2cdbadba
Fernando Lopez Casillas <39224573+flcasillas87@users.noreply.github.com>
2025-08-24 13:25:18 -0600

    Actualizar error.service.ts

--- src/app/error.service.ts
+++ src/app/error.service.ts
@@ -1,1 +1,18 @@
 //maneja un signal global para mostrar mensajes tipo toast o logs
+
+// core/error.service.ts
+import { Injectable, signal } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class ErrorService {
+  errorMessage = signal<string | null>(null);
+
+  setError(message: string) {
+    this.errorMessage.set(message);
+    setTimeout(() => this.clearError(), 5000); // auto-clear después de 5s
+  }
+
+  clearError() {
+    this.errorMessage.set(null);
+  }
+}
